cbf54a3071b712f8af8e2f04b2692469d9cd8b85
Mohammed <junaithdedhai2001@gmail.com>
2025-10-02 19:20:38 +0530

    few unwanted files removed

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  distDir: 'dist',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : '',
+ 
   images: {
     unoptimized: true,
     // domains: ["localhost"],
